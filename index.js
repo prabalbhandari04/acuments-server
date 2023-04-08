1932d66055909860846afe244cf8e935a15c07f5
Prabal Bhandari <54536906+prabalbhandari04@users.noreply.github.com>
2023-04-08 19:02:56 +0530

    atlas added and movie  model created

--- index.js
+++ index.js
@@ -16,3 +16,15 @@ app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define the movie schema
+const movieSchema = new mongoose.Schema({
+    title: String,
+    overview: String,
+    poster_path: String,
+    release_date: Date,
+    vote_average: Number,
+    original_language: String,
+  });
+    // Define the movie model
+  const Movie = mongoose.model('Movie', movieSchema);
